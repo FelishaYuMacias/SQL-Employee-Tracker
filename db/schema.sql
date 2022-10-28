f4b72bfc25feef9223cf17ff6b8f2ddb18956fbd
Felisha <felisha.j.yu@gmail.com>
2022-10-28 16:00:40 -0700

    added foreign keys to tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,21 +5,30 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-id: INT PRIMARY KEY,
-name: VARCHAR(30)
+id INT PRIMARY KEY,
+name VARCHAR(30)
 );
 
 CREATE TABLE role (
-id: INT PRIMARY KEY,
-title: VARCHAR(30)
-salary: DECIMAL,
-department_id: INT
+id INT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id: INT PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT,
-    manager_id: INT
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
